1030b324594b88499a0a66c87d2284a9eb346770
hieu.dotrung <hieu.dotrung@lgepartner.com>
2025-04-28 14:08:31 +0700

    add vim key binding

---
+++ vs_code/user_setting.js
@@ -0,0 +1,19 @@
+// Copy this file into User Setting file
+
+{
+    "vim.normalModeKeyBindingsNonRecursive": [
+    ],
+    "vim.insertModeKeyBindings": [
+        {
+            "before": ["j", "j"],
+            "after": ["<Esc>"]
+        }
+    ],
+    "vim.visualModeKeyBindings": [
+
+        {
+            "before": ["p"],
+            "after": ["\"","_","d","p"]
+        }
+    ],
+}
