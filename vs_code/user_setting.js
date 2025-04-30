8952677d5cb47708c9f14f6bf10dc16e61fc093c
normally96 <normally96@gmail.com>
2025-04-30 11:26:35 +0700

    add gitconfig alias

--- vs_code/user_setting.js
+++ vs_code/user_setting.js
@@ -1,19 +1,32 @@
 // Copy this file into User Setting file
+"vim.normalModeKeyBindingsNonRecursive": [
+],
+"vim.insertModeKeyBindings": [
+	{
+		"before": ["j", "j"],
+		"after": ["<Esc>"]
+	},
+	{
+		"before": ["leader", "c"],
+		"after": ["<Esc>"]
+	}
+],
+"vim.visualModeKeyBindings": [
 
-{
-    "vim.normalModeKeyBindingsNonRecursive": [
-    ],
-    "vim.insertModeKeyBindings": [
-        {
-            "before": ["j", "j"],
-            "after": ["<Esc>"]
-        }
-    ],
-    "vim.visualModeKeyBindings": [
+	{
+		"before": ["p"],
+		"after": ["\"","_","d","p"]
+	},
+	{
+		"before": ["leader", "c"],
+		"after": ["<Esc>"]
+	}
+],
 
-        {
-            "before": ["p"],
-            "after": ["\"","_","d","p"]
-        }
-    ],
-}
+// Copy this to Profile Setting file
+"vim.easymotion": true,
+"vim.incsearch": true,
+"vim.useSystemClipboard": true,
+"vim.useCtrlKeys": true,
+"vim.hlsearch": true,
+"vim.leader": "<space>",
